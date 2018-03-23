5d56979dfe214c9cd499fef36ceb2ab1b98db9b3
Wende Mendes <wende@bluesoft.com.br>
2018-03-23 13:44:10 -0300

    adicionando dependencia de teste no pom xml

---
+++ src/main/java/com/github/arocketman/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.github.arocketman.repositories;
+
+import com.github.arocketman.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * User repository for CRUD operations.
+ */
+public interface UserRepository extends JpaRepository<User,Long> {
+    User findByUsername(String username);
+}
